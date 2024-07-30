2cad6819bc6d73723394fbbe18ec1e8ce0b9adf4
Tejas Ladani <77571495+tejasladani999@users.noreply.github.com>
2024-07-30 21:16:50 +0530

    Create ErrorHandler Class for Handling the Error.

---
+++ utils/ErrorHandler.ts
@@ -0,0 +1,10 @@
+class ErrorHandler extends Error {
+    statusCode: Number;
+    constructor(Message, statusCode:Number){
+        super(Message);
+        this.statusCode = statusCode;
+
+        Error.captureStackTrace(this,this.constructor);
+    }
+}
+module.exports = ErrorHandler;
