1c1781cd2bd63c57d2f432c0c53df2689957bdf3
Tejas Ladani <77571495+tejasladani999@users.noreply.github.com>
2024-08-01 21:57:07 +0530

    Solving an Error related to Errorhandling file.

--- utils/ErrorHandler.ts
+++ utils/ErrorHandler.ts
@@ -1,7 +1,7 @@
 class ErrorHandler extends Error {
     statusCode: Number;
-    constructor(Message, statusCode:Number){
-        super(Message);
+    constructor(message:any, statusCode:Number){
+        super(message);
         this.statusCode = statusCode;
 
         Error.captureStackTrace(this,this.constructor);
