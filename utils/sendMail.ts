95392ef073eb636cc37ee0f24292a2cd943a84bc
Tejas Ladani <77571495+tejasladani999@users.noreply.github.com>
2024-08-05 20:22:13 +0530

    Create Attractive UI for activation-mail, test Activation code sending process.

--- utils/sendMail.ts
+++ utils/sendMail.ts
@@ -1,42 +1,39 @@
-require('dotenv').config();
-import nodemailer,{Transporter} from "nodemailer";
+require("dotenv").config();
+import nodemailer, { Transporter } from "nodemailer";
 import ejs from "ejs";
 import path from "path";
 
-interface EmailOption{
-    email:string;
-    subject:string;
-    template:string;
-    data: {[key:string]:any}
+interface EmailOption {
+  email: string;
+  subject: string;
+  template: string;
+  data: { [key: string]: any };
 }
 
-const sendMail =async (option:EmailOption):Promise <void> => {
-    const transporter: Transporter = nodemailer.createTransport({
-        host: process.env.SMTP_HOST,
-        port: parseInt(process.env.SMTP_PORT || '587'),
-        service: process.env.SMTP_SERVICE,
-        auth:{
-            user:process.env.SMTP_USER,
-            pass:process.env.SMTP_PASSWORD,
-        }
-    });
+const sendMail = async (option: EmailOption): Promise<void> => {
+  const transporter: Transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: parseInt(process.env.SMTP_PORT || "587"),
+    service: process.env.SMTP_SERVICE,
+    auth: {
+      user: process.env.SMTP_MAIL,
+      pass: process.env.SMTP_PASSWORD,
+    },
+  });
 
-    const {email,subject,template,data} = option;
+  const { email, subject, template, data } = option;
+  //get the path to the email template file
+  const templatePath = path.join(__dirname, "../mails", template);
 
-    //get the path to the email template file
-    const templatePath =path.join(__dirname,'..mails',template);
-
-    //Render the email template with EJS
-    const html:string = await ejs.renderFile(templatePath,data);
-
-    const mailOptions ={
-        from:process.env.SMTP_USER,
-        to:email,
-        subject,
-        html
-    };
-
-    await transporter.sendMail(mailOptions);
+  //Render the email template with EJS
+  const html: string = await ejs.renderFile(templatePath, data);
+  const mailOptions = {
+    from: process.env.SMTP_MAIL,
+    to: email,
+    subject,
+    html,
+  };
+  await transporter.sendMail(mailOptions);
 };
 
 export default sendMail;
