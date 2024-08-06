af66e7de404cd276abf58e633e54bf505d83b0d2
Tejas Ladani <77571495+tejasladani999@users.noreply.github.com>
2024-08-06 19:03:48 +0530

    Create User Registration Activationcode functionality, Do the Testing for that as well.

--- controllers/user.controller.ts
+++ controllers/user.controller.ts
@@ -1,6 +1,6 @@
-require ('dotenv').config();
-import { Request,Response, NextFunction } from "express";
-import userModel from "../models/user.model";
+require("dotenv").config();
+import { Request, Response, NextFunction } from "express";
+import userModel, { IUser } from "../models/user.model";
 import ErrorHandler from "../utils/ErrorHandler";
 import { CatchAsyncError } from "../middleware/catchAsyncError";
 import jwt, { Secret } from "jsonwebtoken";
@@ -9,74 +9,121 @@ import path from "path";
 import sendMail from "../utils/sendMail";
 
 // Register User
-interface IRegistrationBody{
-    name: string;
-    email: string;
-    password:string;
-    avatar?:string;
+interface IRegistrationBody {
+  name: string;
+  email: string;
+  password: string;
+  avatar?: string;
 }
 
-export const registrationUser = CatchAsyncError(async(req: Request,res: Response,next: NextFunction) => {
+export const registrationUser = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const { name, email, password } = req.body;
-        const isEmailExist = await userModel.findOne({ email });
-        if(isEmailExist) {
-            return next(new ErrorHandler("Email already exist",400));
-        };
+      const { name, email, password } = req.body;
+      const isEmailExist = await userModel.findOne({ email });
+      if (isEmailExist) {
+        return next(new ErrorHandler("Email already exist", 400));
+      }
 
-        const user: IRegistrationBody = {
-            name,
-            email,
-            password,
-        };
+      const user: IRegistrationBody = {
+        name,
+        email,
+        password,
+      };
 
-        const activationToken = createActivationToken(user);
+      const activationToken = createActivationToken(user);
 
-        const activationCode =activationToken.activationCode;
+      const activationCode = activationToken.activationCode;
 
-        const data = {user: {name:user.name}, activationCode}
-        const html = await ejs.renderFile(
-            path.join(__dirname, "../mails/activation-mail.ejs"),
-            data
-          );
+      const data = { user: { name: user.name }, activationCode };
+      const html = await ejs.renderFile(
+        path.join(__dirname, "../mails/activation-mail.ejs"),
+        data
+      );
 
-        try{
-            await sendMail({
-                email:user.email,
-                subject:"Activation Your Account",
-                template:"activation-mail.ejs",
-                data,
-            });
-            res.status(200).json({
-                success:'true',
-                message: `Plese check your email: ${user.email} to activate your account!`,
-                activationToken: activationToken.token,
-            });
-        } catch(error:any){
-            
-            return next(new ErrorHandler(error.message, 400));
-        }
-    } catch (error:any) {
-        return next(new ErrorHandler(error.message, 409));
+      try {
+        await sendMail({
+          email: user.email,
+          subject: "Activation Your Account",
+          template: "activation-mail.ejs",
+          data,
+        });
+        res.status(200).json({
+          success: "true",
+          message: `Plese check your email: ${user.email} to activate your account!`,
+          activationToken: activationToken.token,
+        });
+      } catch (error: any) {
+        return next(new ErrorHandler(error.message, 400));
+      }
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 409));
     }
-});
-interface IActivationToken{
-    token: string;
-    activationCode: string;
+  }
+);
+interface IActivationToken {
+  token: string;
+  activationCode: string;
 }
 
 export const createActivationToken = (user: any): IActivationToken => {
- const activationCode = Math.floor(1000 + Math.random() * 9000).toString();
-    
- const token = jwt.sign({
-    user,
-    activationCode,
- },
+  const activationCode = Math.floor(1000 + Math.random() * 9000).toString();
 
- process.env.ACTIVATION_SECRET as Secret,
- {
-    expiresIn:"5m",
- });
+  const token = jwt.sign(
+    {
+      user,
+      activationCode,
+    },
 
- return{token, activationCode}
+    process.env.ACTIVATION_SECRET as Secret,
+    {
+      expiresIn: "5m",
+    }
+  );
+
+  return { token, activationCode };
 };
+
+// activate User Account
+
+interface IActivationRequest {
+  activation_token: string;
+  activation_code: string;
+}
+
+export const activateUser = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { activation_token, activation_code } =
+        req.body as IActivationRequest;
+
+      const newUser: { user: IUser; activationCode: string } = jwt.verify(
+        activation_token,
+        process.env.ACTIVATION_SECRET as string
+      ) as { user: IUser; activationCode: string };
+
+      if (newUser.activationCode !== activation_code) {
+        return next(new ErrorHandler("Invalied activation code", 400));
+      }
+
+      const { name, email, password } = newUser.user;
+
+      const existUser = await userModel.findOne({ email });
+
+      if (existUser) {
+        return next(new ErrorHandler("Email Already Exist", 400));
+      }
+      const user = await userModel.create({
+        name,
+        email,
+        password,
+      });
+
+      res.status(201).json({
+        success: true,
+      });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 400));
+    }
+  }
+);
--- routes/user.route.ts
+++ routes/user.route.ts
@@ -1,8 +1,11 @@
 import express from "express";
-import { registrationUser } from "../controllers/user.controller";
+import { activateUser, registrationUser } from "../controllers/user.controller";
+import { actionTypes } from "react-redux-firebase";
 
 const userRouter = express.Router();
 
-userRouter.post('/registration', registrationUser);
+userRouter.post("/registration", registrationUser);
+
+userRouter.post("/activation-user", activateUser);
 
 export default userRouter;
